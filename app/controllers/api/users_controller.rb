d27d97081239d03ae7edb7a4796ff9f01f14e3ba
Adrian Kawanishi <adriankawanishi@gmail.com>
2018-08-21 11:20:16 -0700

    Finish front end user auth

---
+++ app/controllers/api/users_controller.rb
@@ -0,0 +1,15 @@
+class Api::UsersController < ApplicationController
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      login(@user)
+      render json: "api/users/show"
+    else
+      render json: @user.errors_full_messages, status: 422
+    end
+  end
+
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
+end
